6c677da01e3f2c20fc3cc1c186f2dee686b75dde
Usama Nasar <usamanasar38@gmail.com>
2025-03-04 10:21:40 +0500

    feat(api): add user credit table

---
+++ apps/api/prisma/migrations/20250304052122_add_user_credit_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "UserCredit" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserCredit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserCredit_userId_key" ON "UserCredit"("userId");
+
+-- CreateIndex
+CREATE INDEX "UserCredit_userId_idx" ON "UserCredit"("userId");
